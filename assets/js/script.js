4083683f2002c63e64aeffd4acfafe30d23461ad
Jacob Goodall <jraygoodall@gmail.com>
2017-09-28 17:54:35 -0500

    functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-alert('nice');
+$('ul').on('click', 'li', function(){
+  $(this).toggleClass('completed')
+})
+
+$('ul').on('click', 'span', function(e){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  })
+  e.stopPropagation();
+})
+
+$('input[type="text"]').keypress(function(e){
+  if(e.which === 13){
+    var todoText = $(this).val();
+    console.log(todoText);
+    $(this).val('');
+    $('ul').append('<li><span>X </span>' + todoText + '</li>');
+  }
+})
